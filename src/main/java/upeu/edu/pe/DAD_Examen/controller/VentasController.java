5a3d17371e79661d720c54ad08e954f600d3ca77
Emanuel <percypinaud@upeu.edu.pe>
2019-05-30 01:18:17 -0500

    crossOrigin

--- src/main/java/upeu/edu/pe/DAD_Examen/controller/VentasController.java
+++ src/main/java/upeu/edu/pe/DAD_Examen/controller/VentasController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import upeu.edu.pe.DAD_Examen.service.Detalle_VentaService;
 import upeu.edu.pe.DAD_Examen.service.VentasService;
 
 @RequestMapping("/ventas")
+@CrossOrigin(origins = "http://localhost:3000")   
 @RestController
 public class VentasController {
 	@Autowired
